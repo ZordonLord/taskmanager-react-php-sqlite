e3233f77fa5fd4a2d8fe49dea09feb4393af8191
ZordonLord <zordonlord@mail.ru>
2025-07-27 01:56:37 +1000

    Добавлен switch case

--- backend/api.php
+++ backend/api.php
@@ -25,4 +25,76 @@ $id = $parts[1] ?? null;
 
 $input = json_decode(file_get_contents('php://input'), true);
 
+switch ($resource) {
+    case 'projects':
+        switch ($method) {
+            case 'GET':
+                if ($id) {
+                    getProject($pdo, $id);
+                } else {
+                    getAllProjects($pdo);
+                }
+                break;
+            case 'POST':
+                createProject($pdo, $input);
+                break;
+            case 'PUT':
+                if ($id) {
+                    updateProject($pdo, $id, $input);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(['error' => 'Не указан ID проекта']);
+                }
+                break;
+            case 'DELETE':
+                if ($id) {
+                    deleteProject($pdo, $id);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(['error' => 'Не указан ID проекта']);
+                }
+                break;
+            default:
+                http_response_code(405);
+                echo json_encode(['error' => 'Метод не поддерживается']);
+        }
+        break;
 
+    case 'tasks':
+        switch ($method) {
+            case 'GET':
+                if ($id) {
+                    getTask($pdo, $id);
+                } else {
+                    getAllTasks($pdo);
+                }
+                break;
+            case 'POST':
+                createTask($pdo, $input);
+                break;
+            case 'PUT':
+                if ($id) {
+                    updateTask($pdo, $id, $input);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(['error' => 'Не указан ID задачи']);
+                }
+                break;
+            case 'DELETE':
+                if ($id) {
+                    deleteTask($pdo, $id);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(['error' => 'Не указан ID задачи']);
+                }
+                break;
+            default:
+                http_response_code(405);
+                echo json_encode(['error' => 'Метод не поддерживается']);
+        }
+        break;
+
+    default:
+        http_response_code(404);
+        echo json_encode(['error' => 'Неизвестный ресурс']);
+}
